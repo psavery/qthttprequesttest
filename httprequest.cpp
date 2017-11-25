17298801341d1043a951f8bed9ee501d6fde60a4
Patrick Avery <psavery@buffalo.edu>
2017-11-25 16:48:24 -0500

    Improved thread safety and overall design

--- httprequest.cpp
+++ httprequest.cpp
@@ -1,30 +1,75 @@
 #include "httprequest.h"
 
 #include <QDebug>
+#include <QNetworkRequest>
 
-HttpRequest::HttpRequest(QNetworkAccessManager* networkManager,
-                         QObject *parent) :
+HttpRequestManager::HttpRequestManager(
+    const std::shared_ptr<QNetworkAccessManager>& networkManager,
+    QObject* parent) :
  m_networkManager(networkManager),
+ m_requestCounter(0),
  QObject(parent)
 {
-  connect(m_networkManager, &QNetworkAccessManager::finished,
-          this, &HttpRequest::dataReceived);
+  connect(m_networkManager.get(), &QNetworkAccessManager::finished,
+          this, &HttpRequestManager::dataReceived);
 }
 
-void HttpRequest::sendRequest(QUrl url)
+size_t HttpRequestManager::sendRequest(QUrl url)
 {
+  std::unique_lock<std::mutex> lock(m_mutex);
+
   QNetworkRequest request(url);
-  m_networkManager->get(request);
+  QNetworkReply* reply = m_networkManager->get(request);
+  m_pendingReplies[m_requestCounter] = reply;
+
+  ++m_requestCounter;
 }
 
-void HttpRequest::dataReceived(QNetworkReply* pReply)
+bool HttpRequestManager::containsData(size_t i) const
 {
-  m_receivedData = pReply->readAll();
+  std::unique_lock<std::mutex> lock(m_mutex);
+  return m_receivedReplies.find(i) != m_receivedReplies.end();
+}
+
+const QByteArray& HttpRequestManager::data(size_t i) const
+{
+  std::unique_lock<std::mutex> lock(m_mutex);
+
+  static const QByteArray empty = "";
+
+  if (m_receivedReplies.find(i) == m_receivedReplies.end())
+    return empty;
+
+  return m_receivedReplies.at(i);
+}
+
+void HttpRequestManager::dataReceived(QNetworkReply* reply)
+{
+  std::unique_lock<std::mutex> lock(m_mutex);
+
+  // Make sure this HttpRequestManager owns this reply
+  auto it = std::find_if(m_pendingReplies.begin(),
+                         m_pendingReplies.end(),
+                         [reply](const std::pair<size_t, QNetworkReply*>& item)
+                         {
+                           return reply == item.second;
+                         });
+
+  // If not, just return
+  if (it == m_pendingReplies.end())
+    return;
+
+  size_t receivedInd = it->first;
+
+  m_receivedReplies[receivedInd] = reply->readAll();
 
   qDebug() << "Received data!\n";
-  qDebug() << "Recieved data is:\n" << m_receivedData;
+  qDebug() << "Recieved data is:\n" << m_receivedReplies[receivedInd];
 
-  //emit a signal
-  pReply->deleteLater();
-  emit received();
+  m_pendingReplies.erase(receivedInd);
+
+  reply->deleteLater();
+
+  // Emit a signal
+  emit received(receivedInd);
 }
--- httprequest.h
+++ httprequest.h
@@ -1,33 +1,63 @@
 #ifndef HTTPREQUEST_H
 #define HTTPREQUEST_H
 
+#include <memory>
+#include <mutex>
+#include <unordered_map>
+
 #include <QObject>
 #include <QByteArray>
 #include <QNetworkAccessManager>
-#include <QNetworkRequest>
 #include <QNetworkReply>
 
-class HttpRequest : public QObject
+class HttpRequestManager : public QObject
 {
  Q_OBJECT
  public:
-  explicit HttpRequest(QNetworkAccessManager* networkManager,
-                       QObject *parent = nullptr);
-  QByteArray data() const { return m_receivedData; }
+  explicit HttpRequestManager(
+      const std::shared_ptr<QNetworkAccessManager>& networkManager,
+      QObject *parent = nullptr);
 
-  void sendRequest(QUrl url);
+  // Sends a URL request and returns an index that is used to access the
+  // obtained data.
+  size_t sendRequest(QUrl url);
+
+  // Check to see if data has been received for reply index @p i.
+  bool containsData(size_t i) const;
+
+  // Get the received data for a particular reply index @p i. Call
+  // "containsData()" to make sure the data was actually received first.
+  const QByteArray& data(size_t i) const;
+
+  // If data for a particular reply index @p i exists, erase it from the map.
+  void eraseData(size_t i) { m_receivedReplies.erase(i); }
 
  signals:
-  void received();
+  // When a reply is received for a particular index, this signal will be
+  // emitted with the index.
+  void received(size_t);
 
  private slots:
-  void dataReceived(QNetworkReply* pReply);
+  // When the network manager receives a reply, this slot will be activated.
+  // This function will make sure that the reply belongs to this object before
+  // doing anything with it.
+  void dataReceived(QNetworkReply* reply);
 
  private:
-  // A pointer to the network manager. The network manager should not be
-  // deleted until the HttpRequest object is deleted.
-  QNetworkAccessManager* m_networkManager;
-  QByteArray m_receivedData;
+  // A shared pointer to the network access manager.
+  std::shared_ptr<QNetworkAccessManager> m_networkManager;
+
+  // The map of pending replies accessed via their unique index
+  std::unordered_map<size_t, QNetworkReply*> m_pendingReplies;
+
+  // The map of received replies accessed via their unique index
+  std::unordered_map<size_t, QByteArray> m_receivedReplies;
+
+  // This mutex gets locked for all reading and writing operations
+  mutable std::mutex m_mutex;
+
+  // A counter for the request indices
+  size_t m_requestCounter;
 };
 
 #endif // HTTPREQUEST_H
